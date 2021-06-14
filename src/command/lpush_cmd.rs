d77db04a7b1a60755cdfe6ed75c001ec33026afd
lucasveron <veron_lucas@hotmail.com>
2021-06-13 21:57:47 -0300

    impl cmd list lpop,lpush,lrange

--- src/command/lpush_cmd.rs
+++ src/command/lpush_cmd.rs
@@ -3,21 +3,21 @@ use crate::errors::run_error::RunError;
 use crate::server::app_info::AppInfo;
 use crate::server::logger::{Loggable, Logger};
 
-const INFO_COMMAND: &str = "Run command LSET\n";
-const CLIENT_ID: &str = "LSetCommmand";
+const INFO_COMMAND: &str = "Run command LPUSH\n";
+const CLIENT_ID: &str = "LpushCommand";
 
-pub struct LSetCommand {
+pub struct LpushCommand {
     id_job: u32,
     logger: Logger<String>,
 }
 
-impl LSetCommand {
-    pub fn new(id_job: u32, logger: Logger<String>) -> LSetCommand {
-        LSetCommand { id_job, logger }
+impl LpushCommand {
+    pub fn new(id_job: u32, logger: Logger<String>) -> LpushCommand {
+        LpushCommand { id_job, logger }
     }
 }
 
-impl Loggable for LSetCommand {
+impl Loggable for LpushCommand {
     fn get_id_client(&self) -> &str {
         CLIENT_ID
     }
@@ -27,16 +27,16 @@ impl Loggable for LSetCommand {
     }
 }
 
-impl Clone for LSetCommand {
-    fn clone(&self) -> LSetCommand {
-        LSetCommand {
+impl Clone for LpushCommand {
+    fn clone(&self) -> LpushCommand {
+        LpushCommand {
             id_job: self.id_job,
             logger: self.logger.clone(),
         }
     }
 }
 
-impl Command for LSetCommand {
+impl Command for LpushCommand {
     fn run(
         &self,
         args: Vec<&str>,
@@ -47,12 +47,9 @@ impl Command for LSetCommand {
         if let Ok(_r) = log_info_res {}
 
         let db = app_info.get_list_db();
+        //TODO: chequear el caso de que la clave sea de otro tipo
 
-        let mut result = db.lset(
-            args[0].to_string(),
-            args[1].to_string(),
-            args[2].to_string(),
-        )?;
+        let mut result = db.lpush(args).to_string();
         result.push('\n');
 
         Ok(result)
