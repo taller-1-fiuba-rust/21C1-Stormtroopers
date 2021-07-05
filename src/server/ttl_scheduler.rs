361981ccc43bc6ca5709d58758682f3af532707b
Cristian Daniel Queirolo Dominguez <49312550+cqueirolo@users.noreply.github.com>
2021-07-05 19:40:50 -0300

    Feature/ttl bug fix (#91)
    * Added some functionality for filedump
    
    * fmt changes
    
    * Added saving functionality to db
    
    * Finished filedump. Maybe some refactor will be needed in the future.
    
    * Added println to file load
    
    * fmt and clippy changes
    
    * Added check for existing db file
    
    * fmt changes
    
    * Fixed bug with updating the ttl
    
    * fmt and clippy fixes

--- src/server/ttl_scheduler.rs
+++ src/server/ttl_scheduler.rs
@@ -101,7 +101,12 @@ impl TtlScheduler {
     }
 
     pub fn set_ttl(&self, ttl: u64, arg: String) -> Result<String, RunError> {
-        self.set_key_ttl(ttl, arg.clone());
+        match (self.set_key_ttl(ttl, arg.clone())).as_str() {
+            "" => (),
+            old_ttl => {
+                let _ = self.delete_ttl(String::from(old_ttl));
+            }
+        };
 
         let mut key_val = ttl.to_string();
         key_val.push(':');
@@ -187,11 +192,13 @@ impl TtlScheduler {
         let kv_splitted: Vec<&str> = key_val.split(':').collect();
 
         let mut map = map.lock().unwrap();
-        map.insert(
+        match map.insert(
             String::from(kv_splitted[0].trim()),
             kv_splitted[1].trim().parse::<u64>().unwrap(),
-        );
-        String::from(OK)
+        ) {
+            Some(value) => value.to_string(),
+            None => String::from(""),
+        }
     }
 
     fn retrieve_key(
