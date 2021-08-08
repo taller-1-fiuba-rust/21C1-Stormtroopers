661f8a116e3d3ee707c5684284c5a6b5aa0593ca
lucasveron <veron_lucas@hotmail.com>
2021-08-07 23:13:02 -0300

    [ci skip] impl threadpool. remove unwrap, redirect GET invalid resource (#114)
    Approved PR, now merging

--- web_services/src/http_server/router.rs
+++ web_services/src/http_server/router.rs
@@ -1,7 +1,7 @@
 use crate::handlers::process_get_index::process_get_index;
 use crate::handlers::process_not_found::process_not_found;
 use crate::handlers::process_redis::process_redis;
-use crate::http::http_request::{HttpRequest, Method};
+use crate::http::http_request::{HttpRequest, Method, Resource};
 use std::net::TcpStream;
 
 pub struct Router;
@@ -14,9 +14,21 @@ impl Router {
         cmd: String,
         host_port_redis: &str,
     ) {
+        println!("Route request method: {}", cmd);
+        let _idx_resource = Resource::Path("/index".to_string());
         match req.method {
             Method::Get => {
-                process_get_index(stream);
+                println!("GET");
+                match req.resource {
+                    Resource::Path(s) => {
+                        if s == *"/index".to_string() {
+                            process_get_index(stream);
+                        } else {
+                            process_not_found(stream);
+                        };
+                    },
+
+                }
             }
             Method::Post => {
                 process_redis(stream, stream_redis, cmd, host_port_redis);
