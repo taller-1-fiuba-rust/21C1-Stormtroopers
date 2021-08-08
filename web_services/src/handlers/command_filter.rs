514ebe53dfd42a63f2811dae84a009700c290a16
lucasveron <veron_lucas@hotmail.com>
2021-08-08 18:13:27 -0300

    fix commands

--- web_services/src/handlers/command_filter.rs
+++ web_services/src/handlers/command_filter.rs
@@ -1,4 +1,3 @@
-//ver implementación, con match quedaba feo
 pub fn filter_cmd(cmd: String) -> String {
     let cmd_aux = cmd.to_lowercase();
     let cmds: Vec<&str> = cmd_aux.split_ascii_whitespace().collect();
@@ -8,9 +7,10 @@ pub fn filter_cmd(cmd: String) -> String {
         || cmds[0] == "suscribe"
         || cmds[0] == "publish"
         || cmds[0] == "unsuscribe"
+        || cmds[0] == "clear"
         || cmds[0] == "exit"
     {
-        format!("DISABLED COMMAND: {:?} ", cmd)
+        format!("DISABLED COMMAND: {}", cmd)
     } else {
         cmd
     }
